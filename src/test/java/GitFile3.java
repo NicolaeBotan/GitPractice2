6ae1372266ce55e7c2d05382a11923c68e782140
Nicolae <botan.nicolae94@icloud.com>
2024-06-02 09:20:56 -0600

    Adding new Updated Class

--- src/test/java/GitFile3.java
+++ src/test/java/GitFile3.java
@@ -1,5 +1,12 @@
 public class GitFile3 {
     public static void main(String[] args) {
         System.out.println("I am Git Practice of File3");
+        /*
+        git branch -M master
+        git remote add origin https://github.com/NicolaeBotan/GitPractice2.git -- to make the connection
+        -- between your link GitHub repository and your IntelliJ Project
+        git push https://github.com/NicolaeBotan/GitPractice2.git -- when you push the first time
+
+         */
     }
 }
