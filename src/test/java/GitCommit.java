15774fdfb33d3a3865f7bb0ffa6e4dc6917029cc
Nicolae <botan.nicolae94@icloud.com>
2024-06-02 09:44:03 -0600

    Adding the new Changes

--- src/test/java/GitCommit.java
+++ src/test/java/GitCommit.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class GitCommit {
     public static void main(String[] args) {
-        System.out.println("First Modification");
+        //Take a String from User and reverse it
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter any word");
+
+        String word = scanner.nextLine();
+        String reversedWord = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord += word.charAt(i);
+        }
+        System.out.println(reversedWord);
     }
 }
