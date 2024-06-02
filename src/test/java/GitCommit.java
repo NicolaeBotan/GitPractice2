b4d5aff110c08baf16e769cd796da86c5e9003fe
Nicolae <botan.nicolae94@icloud.com>
2024-06-02 09:46:39 -0600

    Adding String Builder

--- src/test/java/GitCommit.java
+++ src/test/java/GitCommit.java
@@ -8,10 +8,10 @@ public class GitCommit {
         System.out.println("Please enter any word");
 
         String word = scanner.nextLine();
-        String reversedWord = "";
+        StringBuilder reversedWord = new StringBuilder();
 
         for (int i = word.length() - 1; i >= 0; i--) {
-            reversedWord += word.charAt(i);
+            reversedWord.append(word.charAt(i));
         }
         System.out.println(reversedWord);
     }
