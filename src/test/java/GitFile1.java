3c480342d0b2b8cb05e3d9e0752a6b19754f1b06
Nicolae <botan.nicolae94@icloud.com>
2024-06-01 18:26:43 -0600

    Adding all The files

---
+++ src/test/java/GitFile1.java
@@ -0,0 +1,6 @@
+public class GitFile1 {
+    public static void main(String[] args) {
+
+        System.out.println("I am Git practice 2");
+    }
+}
---
+++ src/test/java/GitFile2.java
@@ -0,0 +1,8 @@
+public class GitFile2 {
+    public static void main(String[] args) {
+        System.out.println("I am Git Practice 2");
+
+        //git add .  -- means it will add all files
+        //git add scr/test/java/... -- will add this specific Class
+    }
+}
